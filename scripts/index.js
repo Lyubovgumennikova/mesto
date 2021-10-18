dba28e532d08b84d5843dedb8aa73de7cccd3ba3
Lubov Gumennikova <lyubovgumennikova@yandex.ru>
2021-10-18 16:48:32 +0300

    рурар

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,24 @@
 // Находим форму в DOM
-let formElement = // Воспользуйтесь методом querySelector()
+const popupElement = document.querySelector('.popup');// Воспользуйтесь методом querySelector()
+const popupCloseButtonElement = popupElement.querySelector('.popup__close'); 
+const popupOpenButtonElement = document.querySelector('.profile__button-edit');
+
+const togglePopupVisibiliti = function () {
+    popupElement.classList.toggle('popup_is-opened');
+}
+//togglePopupVisibiliti();
+
+popupOpenButtonElement.addEventListener('click', togglePopupVisibiliti);
+popupCloseButtonElement.addEventListener('click', togglePopupVisibiliti);
+// popup_is-opened
 // Находим поля формы в DOM
-let nameInput = // Воспользуйтесь инструментом .querySelector()
-let jobInput = // Воспользуйтесь инструментом .querySelector()
+//let nameInput = // Воспользуйтесь инструментом .querySelector()
+//let jobInput = // Воспользуйтесь инструментом .querySelector()
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+//function formSubmitHandler (evt) {
+//    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                                 // Так мы можем определить свою логику отправки.
                                                 // О том, как это делать, расскажем позже.
 
@@ -16,8 +27,9 @@ function formSubmitHandler (evt) {
     // Выберите элементы, куда должны быть вставлены значения полей
 
     // Вставьте новые значения с помощью textContent
-}
+//}
 
+console.log(togglePopupVisibiliti);
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler); 
+//formElement.addEventListener('submit', formSubmitHandler); 
