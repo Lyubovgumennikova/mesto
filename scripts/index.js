57b97ab5a133922432e5b917d25a7656d29965f4
Lubov Gumennikova <lyubovgumennikova@yandex.ru>
2021-10-31 20:19:57 +0300

    cards

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,53 @@ const popupCloseButtonElement = popupEditElement.querySelector(".popup__close");
 const popupOpenButtonElement = document.querySelector(".profile__button-edit");
 const popupAddButtonElement = document.querySelector(".profile__button-add");
 
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+const cards = document.querySelector('.elements');
+const cardTemplate = document.querySelector('.card-template');
+
+function render() {
+  for (let i = 0; i < initialCards.length; i++) {
+    const element = initialCards[i];
+    renderItem(element);
+  }
+};
+
+function renderItem(element) {
+  const cardElement = cardTemplate.content.cloneNode(true);
+    cardElement.querySelector('.element__text').textContent = element.name;
+    cardElement.querySelector('.element__mask-group').src = element.link;
+
+  cards.prepend(cardElement);
+};
+render()
+
 const profilElement = document.querySelector(".profile");
 let nameProfile = profilElement.querySelector(".profile__info-name");
 let jobProfile = profilElement.querySelector(".profile__info-job");
@@ -32,6 +79,10 @@ const closeProfPopup = function (){
   popupEditElement.classList.remove('popup_opened');
 };
 
+const closeCardPopup = function (){
+  popupCardElement.classList.remove('popup_opened');
+};
+
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
@@ -46,3 +97,4 @@ formElement.addEventListener('submit', formSubmitHandler);
 popupOpenButtonElement.addEventListener("click", openProfPopup);
 popupAddButtonElement.addEventListener("click", openCardPopup);
 popupCloseButtonElement.addEventListener("click", closeProfPopup);
+
